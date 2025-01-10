c79f422239e8588af37ed92cd9b2d34202167b64
Liam Howell <production3.8k37+liam@coreelectronics.com.au>
2025-01-10 15:09:09 +1100

    Finish development

--- PiicoDev_MMC5603.py
+++ PiicoDev_MMC5603.py
@@ -1,9 +1,59 @@
+# Class and methods for the MMC5603 3-axis magnetometer.
+# Written by Michael Ruppe and Liam Howell at Core Electronics
+
+# 2025 JAN 10 - Initial release
+
+
 from PiicoDev_Unified import *
 import math
 import time
 
+compat_str = '\nUnified PiicoDev library out of date.  Get the latest module: https://piico.dev/unified \n'
+range_compat_str = "Setting the range is not directly configurable, this has been left as a placeholder for compatability"
+_I2C_ADDRESS = 0x30
+
+# Registers
+_REG_XOUT0 = 0x00  # Xout[19:12]
+_REG_XOUT1 = 0x01  # Xout[11:4]
+_REG_YOUT0 = 0x02  # Yout[19:12]
+_REG_YOUT1 = 0x03  # Yout[11:4]
+_REG_ZOUT0 = 0x04  # Zout[19:12]
+_REG_ZOUT1 = 0x05  # Zout[11:4]
+_REG_XOUT2 = 0x06  # Xout[3:0]
+_REG_YOUT2 = 0x07  # Yout[3:0]
+_REG_ZOUT2 = 0x08  # Zout[3:0]
+_REG_TEMP = 0x09   # Temperature output
+_REG_STATUS = 0x18 # Status register
+_REG_ODR = 0x1A    # Output data rate
+_REG_CTRL0 = 0x1B  # Internal control 0
+_REG_CTRL1 = 0x1C  # Internal control 1
+_REG_CTRL2 = 0x1D  # Internal control 2
+_REG_PRODUCT_ID = 0x39  # Product ID
+
+# Control bits
+_BIT_TAKE_MEAS_M = 0x01
+_BIT_TAKE_MEAS_T = 0x02
+_BIT_DO_SET = 0x08
+_BIT_DO_RESET = 0x10
+_BIT_AUTO_SR = 0x04
+
+def _readBit(x, n):
+    return x & 1 << n != 0
+
+def _setBit(x, n):
+    return x | (1 << n)
+
+def _clearBit(x, n):
+    return x & ~(1 << n)
+
+def _writeBit(x, n, b):
+    if b == 0:
+        return _clearBit(x, n)
+    else:
+        return _setBit(x, n)
+
 class PiicoDev_MMC5603(object):
-    def __init__(self, bus=None, freq=None, sda=None, scl=None, addr=0x30, odr=100, calibrationFile='calibration.cal', suppress_warnings=False):
+    def __init__(self, bus=None, freq=None, sda=None, scl=None, addr=_I2C_ADDRESS, odr=255, sign_x=0, sign_y=1, sign_z=1, calibrationFile='calibration.cal', range=None, suppress_warnings=False):
         try:
             if compat_ind >= 1:
                 pass
@@ -13,95 +63,165 @@ class PiicoDev_MMC5603(object):
             print(compat_str)
         self.i2c = create_unified_i2c(bus=bus, freq=freq, sda=sda, scl=scl)
         self.addr = addr
-        self._ctrl2_cache = 0x00
-        
         self.calibrationFile = calibrationFile
         self.suppress_warnings = suppress_warnings
-        
+
         self.x_offset = 0
         self.y_offset = 0
         self.z_offset = 0
+        if sign_x == 0:
+            self.sign_x = -1
+        else:
+            self.sign_x = 1
+        if sign_y == 0:
+            self.sign_y = -1
+        else:
+            self.sign_y = 1
+        if sign_z == 0:
+            self.sign_z = -1
+        else:
+            self.sign_z = 1
+        self.sensitivity = 0.1 # Changes depending on bit-mode, 20bit(0.0625)
+        
+        if range is not None:
+            print(range_compat_str)
+
+        self.odr = odr
+
         self.declination = 0
         self.data = {}
+        self._dataValid = False
 
         # Perform initialization
-        self.reset()
-        self.setOutputDataRate(odr)
-        self.enableContinuousMode()
+        try:
+            self.check_ID()
+            self.reset()
+            self.setOutputDataRate(self.odr)
+            self.enable_continuous_mode()
+            
+        except Exception as e:
+            print(i2c_err_str.format(self.addr))
+            raise e
+
         if calibrationFile is not None:
             self.loadCalibration()
         sleep_ms(5)
 
     def reset(self):
-        """Reset the sensor."""
-        self.i2c.writeto_mem(self.addr, 0x1C, bytes([0x80]))
-        time.sleep(0.02)
+        self.i2c.writeto_mem(self.addr, _REG_CTRL1, bytes([0x80]))
+        sleep_ms(20)
+        self.set_reset()
+
+    def check_ID(self):
+        id = self.i2c.readfrom_mem(self.addr, _REG_PRODUCT_ID, 1)[0]
+        if id != 0x10:  # Expected product ID for MMC5603
+            print(f"Warning: Unexpected product ID: {id}")
 
     def setOutputDataRate(self, odr):
         """Set the output data rate."""
         if not (1 <= odr <= 255):
             raise ValueError("ODR must be between 1 and 255.")
-        self.i2c.writeto_mem(self.addr, 0x1A, bytes([odr]))
+        self.i2c.writeto_mem(self.addr, _REG_ODR, bytes([odr]))
+
+    def _convertAngleToPositive(self, angle):
+        if angle >= 360.0:
+            angle = angle - 360.0
+        if angle < 0:
+            angle = angle + 360.0
+        return angle
 
     def setRange(self, range):
-        """Set the range (not directly configurable, placeholder for compatibility)."""
-        pass  # MMC5603 has a fixed range; this function is provided for interface consistency.
+        if not self.suppress_warnings:
+            print(range_compat_str)
+        pass
 
-    def enableContinuousMode(self):
-        """Enable continuous measurement mode."""
-        self.i2c.writeto_mem(self.addr, 0x1B, bytes([0x80]))  # Enable CMM frequency
-        self._ctrl2_cache |= 0x10  # Enable continuous mode
-        self.i2c.writeto_mem(self.addr, 0x1D, bytes([self._ctrl2_cache]))
+    def enable_continuous_mode(self):
+        self.i2c.writeto_mem(self.addr, _REG_CTRL0, bytes([0x80]))
+        self.i2c.writeto_mem(self.addr, _REG_CTRL2, bytes([0x10]))
+
+    def set_BW(self, BW= 0x03):
+        self.i2c.writeto_mem(self.addr, _REG_CTRL1, bytes([BW]))  # BW = 11, default
+        
+    def readStatus(self):
+        status = int.from_bytes(self.i2c.readfrom_mem(self.addr, 0x18, 1), 'big')
+#         print(bin(status))
+        return status
+    
+    def setDeclination(self, dec):
+        self.declination = dec
+    
+    def _measurementStatus(self):
+        status = self.i2c.readfrom_mem(self.addr, _REG_STATUS, 1)[0]
+        if status & 0x02:  # Check Meas_m_done bit
+            return True
+        else:
+            return False
+
+    def dataValid(self):
+        return self._dataValid
+
+    def set_reset(self):
+        """Perform SET and RESET operations to eliminate offset errors and residual magnetization."""
+        # Perform SET operation
+        self.i2c.writeto_mem(self.addr, _REG_CTRL0, bytes([_BIT_DO_SET]))
+        sleep_ms(1)  # Wait for at least 1 ms
+
+        # Perform RESET operation
+        self.i2c.writeto_mem(self.addr, _REG_CTRL0, bytes([_BIT_DO_RESET]))
+        sleep_ms(1)  # Wait for at least 1 ms
 
     def read(self, raw=False):
         """Read raw magnetic field data in microteslas (uT)."""
-        data_ready = False
-        for _ in range(10):  # Timeout loop
-            status = int.from_bytes(self.i2c.readfrom_mem(self.addr, 0x18, 1), 'big')
-            if status & 0x04:  # Data ready bit
-                data_ready = True
-                break
-            time.sleep(0.001)
+        self._dataValid = False
+        NaN = {'x':float('NaN'),'y':float('NaN'),'z':float('NaN')}
+        self.sample = NaN
+        # Read all data registers
+        data = self.i2c.readfrom_mem(self.addr, _REG_XOUT0, 9)
+        x = (data[0] << 8) | (data[1])
+        y = (data[2] << 8) | (data[3])
+        z = (data[4] << 8) | (data[5])
+
+        x -= 1 << 15
+        y -= 1 << 15
+        z -= 1 << 15
         
-#         if not data_ready:
-#             raise RuntimeError("Data not ready.")
-        # Read 9 bytes of raw magnetic data
-        raw_data = self.i2c.readfrom_mem(self.addr, 0x00, 9)
-        x = ((raw_data[0] << 12) | (raw_data[1] << 4) | (raw_data[6] >> 4)) - (1 << 19)
-        y = ((raw_data[2] << 12) | (raw_data[3] << 4) | (raw_data[7] >> 4)) - (1 << 19)
-        z = ((raw_data[4] << 12) | (raw_data[5] << 4) | (raw_data[8] >> 4)) - (1 << 19)
-
-        if raw:
-            return {'x': x, 'y': y, 'z': z}
-        else:
-            scale = 0.00625  # Conversion factor to microteslas
-            return {'x': (x  - self.x_offset)* scale, 'y': (y - self.y_offset) * scale , 'z': (z - self.z_offset) * scale }
-
-    def readMagnitude(self):
-        """Calculate the magnitude of the magnetic field."""
-        data = self.read()
-        return math.sqrt(data['x'] ** 2 + data['y'] ** 2 + data['z'] ** 2)
-
-    def readHeading(self):
-        """Calculate the heading (angle in degrees)."""
-        data = self.read()
-        heading = math.atan2(data['y'], data['x']) * (180 / math.pi)
-        if heading < 0:
-            heading += 360
-        return heading
+        if (x >= 0x8000):
+            x = -((65535 - x) + 1)
+        x = (x - self.x_offset)
+        if (y >= 0x8000):
+            y = -((65535 - y) + 1)
+        y = (y - self.y_offset)
+        if (z >= 0x8000):
+            z = -((65535 - z) + 1)
+        z = (z - self.z_offset)
+        if raw is False:
+            x *= (self.sensitivity*self.sign_x)
+            y *= (self.sensitivity*self.sign_y)
+            z *= (self.sensitivity*self.sign_z)
+        self._dataValid = True
+        self.sample = {'x':x,'y':y,'z':z}
+        return self.sample
 
     def readPolar(self):
-        """Read the magnitude and heading."""
-        magnitude = self.readMagnitude()
-        heading = self.readHeading()
-        return {'magnitude': magnitude, 'heading': heading}
+        cartesian = self.read()
+        angle = ( math.atan2(cartesian['x'],-cartesian['y']) /math.pi)*180.0 + self.declination
+        angle = self._convertAngleToPositive(angle)
+        magnitude = math.sqrt(cartesian['x']*cartesian['x'] + cartesian['y']*cartesian['y'] + cartesian['z']*cartesian['z'])
+        return {'polar':angle, 'Gauss':magnitude*100, 'uT':magnitude}
 
-    def calibrate(self, enable_logging=False, disable_z=True):
-        try:
-            self.setOutputDataRate(3)
-        except Exception as e:
-            print(i2c_err_str.format(self.addr))
-            raise e
+    def readMagnitude(self):
+        return self.readPolar()['uT']
+
+    def readHeading(self):
+        return self.readPolar()['polar']
+    
+    def setDeclination(self, dec):
+        self.declination = dec
+
+    def calibrate(self, enable_logging=False, disable_z=False):
+        self.x_offset = 0
+        self.y_offset = 0
+        self.z_offset = 0
         x_min = 65535
         x_max = -65535
         y_min = 65535
@@ -115,8 +235,10 @@ class PiicoDev_MMC5603(object):
         i = 0
         x=0;y=0;z=0;
         a=0.5 # EMA filter weight
+        iterator = 0
         while i < range:
             i += 1
+            iterator += 1
             sleep_ms(5)
             d = self.read(raw=True)
             x = a*d['x'] + (1-a)*x # EMA filter
@@ -130,7 +252,9 @@ class PiicoDev_MMC5603(object):
                 if z < z_min: z_min = z; i=0
                 if z > z_max: z_max = z; i=0
             j = round(10*i/range);
-            print( '\015[' + int(j)*'*' + int(10-j)*' ' + ']'+'     ' +(str(d['x']) + ',' + str(d['y']) + ',' + str(d['z'])), end='') # print a progress bar
+            if iterator > 10:
+                iterator = 0
+                print( '\015[' + int(j)*'*' + int(10-j)*' ' + ']'+'     ' +(str(i)), end='') # print a progress bar
             if enable_logging:
                 log = log + (str(d['x']) + ',' + str(d['y']) + ',' + str(d['z']) + '\n')
 #         self.setOutputDataRate(self.odr) # set the output data rate back to the user selected rate
@@ -145,6 +269,7 @@ class PiicoDev_MMC5603(object):
             flog = open("calibration.log", "w")
             flog.write(log)
             flog.close
+
 
     def loadCalibration(self):
         try:
--- README.md
+++ README.md
@@ -1,13 +1,73 @@
-# PiicoDev® Magnetometer MMC5603 MicroPython Module
+# PiicoDev® Magnetometer QMC6310 MicroPython Module
 
-
-This is the firmware repo for the [Core Electronics PiicoDev® Magnetometer MMC5603](https://core-electronics.com.au/catalog/product/view/sku/XXXXXX)
+This is the firmware repo for the [Core Electronics PiicoDev® Magnetometer MMC5603](https://core-electronics.com.au/catalog/product/view/sku/CE10107)
 
 This module depends on the [PiicoDev Unified Library](https://github.com/CoreElectronics/CE-PiicoDev-Unified), include `PiicoDev_Unified.py` in the project directory on your MicroPython device.
 
+See the [Quickstart Guides](https://piico.dev/p34)
 
-See the [Quickstart Guide](https://piico.dev/pX)
+## Details
+### `PiicoDev_MMC5603(bus=, freq=, sda=, scl=, addr=0x30, odr=255V,  range=, sign_x=0, sign_y=1, sign_z=1, cal_filename='calibration.cal')`
 
+
+Parameter | Type | Range            | Default                               | Description
+--------- | ---- | ---------------- | ------------------------------------- | --------------------------------------------------
+bus       | int  | 0, 1             | Raspberry Pi Pico: 0, Raspberry Pi: 1 | I2C Bus.  Ignored on Micro:bit
+freq      | int  | 100-1000000      | Device dependent                      | I2C Bus frequency (Hz).  Ignored on Raspberry Pi
+sda       | Pin  | Device Dependent | Device Dependent                      | I2C SDA Pin. Implemented on Raspberry Pi Pico only
+scl       | Pin  | Device Dependent | Device Dependent                      | I2C SCL Pin. Implemented on Raspberry Pi Pico only
+addr      | int  | 0x30             | 0x30                                  | This address cannot be changed
+odr       | int  | 0 - 255          | 255                                   | Range from 0-  255 Hz
+range     | int  | 3000             | 3000                                  | Not user configurable, Kept for compatability.
+sign_x    | int  | 0, 1             | 0                                     | Sign to represent the polarity of the magnetic field. 0 Matches the silk screen
+sign_y    | int  | 0, 1             | 1                                     | Sign to represent the polarity of the magnetic field. 1 Matches the silk screen
+sign_z    | int  | 0, 1             | 1                                     | Sign to represent the polarity of the magnetic field. 1 Matches the silk screen
+cal_filename | string |  | 'calibration.cal' | If more than one magnetometer (for example on seperate I2C buses or if an I2C mux is used), use a different filename for each. If set to `None` calibration is skipped.
+
+### `PiicoDev_MMC5603.readMagnitude()`
+Reads the magnetic field magnitude using the calibration generated during the calibration routine if available.
+Parameter   | Type  | Range                    | Description | Unit
+----------- | ----- | ------------------------ | ----------- | ------
+returned    | float | 0.0 to 3000.0            | Magnitude field strength | uT
+
+### `PiicoDev_MMC5603.readHeading()`
+Reads the magnetic field bearing from true north.  If no declination is provided the result is a bearing from magnetic north.  Uses the calibration generated during the calibration routine if available.
+Parameter   | Type  | Range        | Description | Unit
+----------- | ----- | ------------ | ----------- | ------
+returned    | float | 0.0 to 360.0 | Bearing from true north or magnetic north if no declination is set | deg
+
+### `PiicoDev_MMC5603.readPolar()`
+Reads the raw magnetic field magnitude and angle (degrees) in the X and Y plane.
+Parameter   | Type  | Range                   | Description | Unit
+----------- | ----- | ----------------------- | ----------- | ----
+**Returns** | **Dictionary**
+polar       | float | 0.0 to 360.0            | Raw bearing from magnetic north | deg
+Gauss       | float | 0.0 to 3.0              | Magnetic field strength | Gauss
+returned    | float | 0.0 to 3000.0            | Magnitude field strength | uT
+
+### `PiicoDev_MMC5603.read()`
+Reads the X, Y and Z components of the magnetic field.
+Parameter   | Type | Range           | Description
+----------- | ---- | --------------- | -----------
+**Returns** | **Dictionary**
+x           | float  | 0.0 to 3000.0 | X magnetic field component
+y           | float  | 0.0 to 3000.0 | Y magnetic field component
+z           | float  | 0.0 to 3000.0 | Z magnetic field component
+
+### `PiicoDev_MMC5603.calibrate()`
+Routine to calibrate the magnetometer.  Rotate the magnetometer in the X and Y or X, Y, & Z directions until the routine is complete.
+
+### `PiicoDev_MMC5603.setOutputDataRate(odr)`
+Sets the Output Data Rate.
+Parameter | Type | Range  | Description
+--------- | ---- | ------ | -----------
+odr       | int  | 0 to 255 | Set the data rate in updates per second (Hz)
+
+### `PiicoDev_MMC5603.setRange(range)`
+A function left in for compatability
+Parameter | Type | Range  | Description
+--------- | ---- | ------ | -----------
+range     | int  | -inf to inf | Prints a string regarding compatability
 
 # License
 This project is open source - please review the LICENSE.md file for further licensing information.
